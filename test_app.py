f91051170421082fd36d8269209b4ec4818bdcdc
CenTos-FS <santosh.mahto1906@gmail.com>
2020-07-27 07:07:47 +0530

    Test cases of 200 and 404 status code for GET '/actors' endpoint

--- test_app.py
+++ test_app.py
@@ -1,1 +1,41 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+from app import create_app
+from models import setup_db, Actors, Movies
+import config
 
+class CapstoneTestCases(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_path = config.SQLALCHEMY_DATABASE_URI
+        setup_db(self.app, self.database_path)
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+    
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    def test_get_actor(self):
+        response = self.client().get('/actors')
+        data = json.loads(response.data)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['actors'])
+
+    def test_get_actor_404(self):
+        response = self.client().get('/actors?page=1000')   
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource not found') 
